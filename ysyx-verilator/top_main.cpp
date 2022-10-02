130af260946ee47a1784ecf9b647f32199862986
myyerrol <myyerrol@126.com>
2022-10-02 18:38:06 +0800

    feat: add verilator task

---
+++ ysyx-verilator/top_main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <verilated.h>
+#include "Vtop.h"
+
+int main(int argc, char** argv, char** env) {
+    if (false && argc && argv && env) {
+    }
+
+    Vtop* top = new Vtop;
+
+    while (!Verilated::gotFinish()) {
+        int a = rand() & 1;
+        int b = rand() & 1;
+        top->a = a;
+        top->b = b;
+        top->eval();
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+        assert(top->f == (a ^ b));
+    }
+}
