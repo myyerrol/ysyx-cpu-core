1d7fb90f8603667bd8c70c6ebf35d14204ae1418
myyerrol <myyerrol@126.com>
2022-10-23 21:44:54 +0800

    feat: add clk signal

--- ysyx-digitalcircuit/6_shifter/csrc/shifter_top.cpp
+++ ysyx-digitalcircuit/6_shifter/csrc/shifter_top.cpp
@@ -5,13 +5,20 @@ static TOP_NAME dut;
 
 void nvboard_bind_all_pins(Vshifter_top* shifter_top);
 
+static void single_cycle() {
+    dut.i_clk = 0;
+    dut.eval();
+    dut.i_clk = 1;
+    dut.eval();
+}
+
 int main() {
     nvboard_bind_all_pins(&dut);
     nvboard_init();
 
     while(1) {
         nvboard_update();
-        dut.eval();
+        single_cycle();
     }
 
     nvboard_quit();
--- ysyx-digitalcircuit/6_shifter/vsrc/shifter.v
+++ ysyx-digitalcircuit/6_shifter/vsrc/shifter.v
@@ -3,8 +3,10 @@ module shifter(
     input  wire [7:0] i_num,
     output reg  [7:0] o_num
 );
+    parameter CLK_NUM = 5000000;
 
-    reg [0:0] t_bit = 1'b0;
+    reg [31:0] t_count = 32'h00;
+    reg [0:0]  t_bit   = 1'b0;
 
     initial begin
         o_num = i_num;
@@ -13,7 +15,13 @@ module shifter(
     assign t_bit = o_num[4] ^ o_num[3] ^ o_num[2] ^ o_num[0];
 
     always @(posedge i_clk) begin
-        o_num <= { t_bit, o_num[7:1] };
+        if (t_count == CLK_NUM) begin
+            t_count <= 32'h00;
+            o_num <= { t_bit, o_num[7:1] };
+        end
+        else begin
+            t_count <= t_count + 1;
+        end
     end
 
 endmodule
