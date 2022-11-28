85d405eb8f12fbdd452489bf80a040ee54514921
myyerrol <myyerrol@126.com>
2022-11-28 22:05:36 +0800

    feat: add strrpc function

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -76,9 +76,27 @@ static int cmd_info(char *args) {
   return 0;
 }
 
+static char *strrpc(char *str, char *str_old, char *str_new) {
+  char str_buf[strlen(str)];
+  memset(str_buf, 0, sizeof(str_buf));
+  for (int i = 0; i < strlen(str); i++) {
+    if (!strncmp(str + i, str_old, strlen(str_old))) {
+      strcat(str_buf, str_new);
+      i += strlen(str_old) - 1;
+    }
+    else {
+      strncat(str_buf, str + i, 1);
+    }
+  }
+  strcpy(str, str_buf);
+  return str;
+}
+
 static int cmd_x(char *args) {
   char *args_n = strtok(args, " ");
   char *args_addr = strtok(NULL, " ");
+  strrpc(args_addr, "0x", "");
+  Log("%s", args_addr);
   if (args_n != NULL && args_addr != NULL) {
     uint32_t n = atoi(args_n);
     uint32_t addr = strtol(args_addr, NULL, 16);
