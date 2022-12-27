3e824a7a76aca330fd5703ec4e24a8b89d7383ca
myyerrol <myyerrol@126.com>
2022-12-27 21:16:49 +0800

    feat: add inst printf

--- nemu/include/utils.h
+++ nemu/include/utils.h
@@ -74,6 +74,10 @@ uint64_t get_time();
 // utils
 char *strrpc(char *str, char *str_old, char *str_new);
 
+#define PRINTF_BIN_PATTERN_INT2 "%c%c"
+#define PRINTF_BIN_PATTERN_INT3 "%c%c%c "
+#define PRINTF_BIN_PATTERN_INT5 "%c%c%c%c%c "
+#define PRINTF_BIN_PATTERN_INT7 "%c%c%c%c%c%c%c "
 #define PRINTF_BIN_PATTERN_INT8 "%c%c%c%c%c%c%c%c "
 #define PRINTF_BIN_PATTERN_INT16 \
   PRINTF_BIN_PATTERN_INT8 PRINTF_BIN_PATTERN_INT8
@@ -81,7 +85,31 @@ char *strrpc(char *str, char *str_old, char *str_new);
   PRINTF_BIN_PATTERN_INT16 PRINTF_BIN_PATTERN_INT16
 #define PRINTF_BIN_PATTERN_INT64 \
   PRINTF_BIN_PATTERN_INT32 PRINTF_BIN_PATTERN_INT32
-
+#define PRINTF_BIN_PATTERN_INST \
+  PRINTF_BIN_PATTERN_INT7 PRINTF_BIN_PATTERN_INT5 PRINTF_BIN_PATTERN_INT5 \
+  PRINTF_BIN_PATTERN_INT3 PRINTF_BIN_PATTERN_INT5 PRINTF_BIN_PATTERN_INT5 \
+  PRINTF_BIN_PATTERN_INT2
+#define PRINTF_BIN_INT2(i)      \
+  (((i) & 0x02ll) ? '1' : '0'), \
+  (((i) & 0x01ll) ? '1' : '0')
+#define PRINTF_BIN_INT3(i)      \
+  (((i) & 0x04ll) ? '1' : '0'), \
+  (((i) & 0x02ll) ? '1' : '0'), \
+  (((i) & 0x01ll) ? '1' : '0')
+#define PRINTF_BIN_INT5(i)      \
+  (((i) & 0x10ll) ? '1' : '0'), \
+  (((i) & 0x08ll) ? '1' : '0'), \
+  (((i) & 0x04ll) ? '1' : '0'), \
+  (((i) & 0x02ll) ? '1' : '0'), \
+  (((i) & 0x01ll) ? '1' : '0')
+#define PRINTF_BIN_INT7(i)      \
+  (((i) & 0x40ll) ? '1' : '0'), \
+  (((i) & 0x20ll) ? '1' : '0'), \
+  (((i) & 0x10ll) ? '1' : '0'), \
+  (((i) & 0x08ll) ? '1' : '0'), \
+  (((i) & 0x04ll) ? '1' : '0'), \
+  (((i) & 0x02ll) ? '1' : '0'), \
+  (((i) & 0x01ll) ? '1' : '0')
 #define PRINTF_BIN_INT8(i)      \
   (((i) & 0x80ll) ? '1' : '0'), \
   (((i) & 0x40ll) ? '1' : '0'), \
@@ -97,3 +125,8 @@ char *strrpc(char *str, char *str_old, char *str_new);
   PRINTF_BIN_INT16((i) >> 16), PRINTF_BIN_INT16(i)
 #define PRINTF_BIN_INT64(i) \
   PRINTF_BIN_INT32((i) >> 32), PRINTF_BIN_INT32(i)
+#define PRINTF_BIN_INST(i) \
+  PRINTF_BIN_INT7((i) >> 25), PRINTF_BIN_INT5((i) >> 20), \
+  PRINTF_BIN_INT5((i) >> 15), PRINTF_BIN_INT3((i) >> 12), \
+  PRINTF_BIN_INT5((i) >> 7),  PRINTF_BIN_INT5((i) >> 5),  \
+  PRINTF_BIN_INT2(i)
