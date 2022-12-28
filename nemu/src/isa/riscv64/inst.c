dc605954004e686d53e8bd1d69a7b9d910496354
myyerrol <myyerrol@126.com>
2022-12-28 21:21:30 +0800

    feat: add some inst

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -32,17 +32,20 @@ enum {
   TYPE_N
 };
 
-#define src1R() do { *src1 = R(rs1); } while (0)
-#define src2R() do { *src2 = R(rs2); } while (0)
-#define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while (0)
+#define src1R() do { *src1 = R(rs1); } while (0);
+#define src2R() do { *src2 = R(rs2); } while (0);
+#define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while (0);
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | \
-                                 BITS(i, 11, 7); } while (0)
-#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while (0)
-// #define immJ() do { *imm = SEXT(BITS(i, 31, 21), 11) << 1; } while (0)
+                                 BITS(i, 11, 7); } while (0);
+#define immB() do { *imm = SEXT(((BITS(i, 31, 31) << 11) | \
+                                 (BITS(i, 7, 7) << 10) | \
+                                 (BITS(i, 30, 25) << 4) | \
+                                  BITS(i, 11, 8)), 12) << 1; } while (0);
+#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while (0);
 #define immJ() do { *imm = SEXT(((BITS(i, 20, 20) << 19) | \
                                  (BITS(i, 19, 12) << 11) | \
                                  (BITS(i, 20, 20) << 10) | \
-                                  BITS(i, 30, 21)), 20) << 1; } while (0)
+                                  BITS(i, 30, 21)), 20) << 1; } while (0);
 
 static void decode_operand(Decode *s,
                            int *dest,
@@ -56,8 +59,10 @@ static void decode_operand(Decode *s,
   int rs2 = BITS(i, 24, 20);
   *dest = rd;
   switch (type) {
+    case TYPE_R: src1R(); src2R();         break;
     case TYPE_I: src1R();          immI(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_J:                   immJ(); break;
   }
@@ -86,14 +91,18 @@ static int decode_exec(Decode *s) {
   printf("pc:   " FMT_WORD "\n", s->pc);
   printf("dnpc: " FMT_WORD "\n", s->dnpc);
 
-  INSTPAT("??????? ????? ????? 000 ????? 00100 11",
-          addi,
-          I,
-          R(dest) = src1 + imm);
   INSTPAT("??????? ????? ????? ??? ????? 00101 11",
           auipc,
           U,
           R(dest) = s->pc + imm);
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11",
+          addi,
+          I,
+          R(dest) = src1 + imm);
+  INSTPAT("??????? ????? ????? 000 ????? 00110 11",
+          addiw,
+          I,
+          R(dest) = SEXT(BITS(src1 + imm, 31, 0), 32));
   INSTPAT("??????? ????? ????? ??? ????? 11011 11",
           jal,
           J,
@@ -104,7 +113,14 @@ static int decode_exec(Decode *s) {
           I,
           R(dest) = s->pc + 4;
           s->dnpc = ((src1 + imm) & -1));
-
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11",
+          beq,
+          B,
+          s->dnpc = (src1 == src2) ? (s->pc + imm) : s->dnpc);
+  INSTPAT("??????? ????? ????? 001 ????? 11000 11",
+          bne,
+          B,
+          s->dnpc = (src1 != src2) ? (s->pc + imm) : s->dnpc);
   INSTPAT("??????? ????? ????? 011 ????? 00000 11",
           ld,
           I,
@@ -113,11 +129,27 @@ static int decode_exec(Decode *s) {
           sd,
           S,
           Mw(src1 + imm, 8, src2));
+  INSTPAT("??????? ????? ????? 011 ????? 00100 11",
+          sltiu,
+          I,
+          R(dest) = (src1 < imm) ? 1 : 0);
+
+  INSTPAT("0000000 ????? ????? 000 ????? 01100 11",
+          add,
+          R,
+          R(dest) = src1 + src2);
+  INSTPAT("0100000 ????? ????? 000 ????? 01100 11",
+          sub,
+          R,
+          R(dest) = src1 - src2);
+
+
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11",
           ebreak,
           N,
           NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+
   INSTPAT("??????? ????? ????? ??? ????? ????? ??",
           inv,
           N,
