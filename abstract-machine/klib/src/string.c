834c220f277002296d6cb855f06c7072acab7e05
myyerrol <myyerrol@126.com>
2023-03-20 19:03:50 +0800

    feat: pass string test

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,90 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  // assert(s != NULL);
+  size_t len = 0;
+  while (*s++ != '\0') {
+    len++;
+  }
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  // assert((dst != NULL) && (src != NULL));
+  char *res = dst;
+  while ((*dst++ = *src++) != '\0');
+  return res;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  // assert((dst != NULL) && (src != NULL));
+  char *res = dst;
+  size_t i = 0;
+  while ((*dst++ = *src++) != '\0' && i++ < n);
+  if (*(--dst) != '\0') {
+    *dst = '\0';
+  }
+  return res;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  // assert((dst != NULL) && (src != NULL));
+  char *res = dst;
+  dst = dst + strlen(dst);
+  while ((*dst++ = *src++) != '\0');
+  return res;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  // assert((s1 != NULL) && (s2 != NULL));
+  while (*s1 == *s2) {
+    if (*s1 == '\0') {
+      return 0;
+    }
+    else {
+      s1++;
+      s2++;
+    }
+  }
+  return *s1 - *s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  // assert((s1 != NULL) && (s2 != NULL));
+  size_t i = 0;
+  while (i++ < n) {
+    if (*s1 == *s2) {
+      s1++;
+      s2++;
+    }
+    else {
+      return *s1 - *s2;
+    }
+  }
+
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  // assert(s != NULL);
+  size_t i = 0;
+  char *res = (char *)s;
+  while (*res != '\0' && i++ < n) {
+    *res++ = c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  return strncpy(dst, src, n);
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  return strncpy(out, in, n);
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  return strncmp(s1, s2, n);
 }
 
 #endif
