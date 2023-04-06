0c9a3a5811f5187d7270786163305940b3efa3e4
myyerrol <myyerrol@126.com>
2023-04-06 16:42:45 +0800

    style: solve fread return value problem

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -22,7 +22,7 @@ CXX := g++
 endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
-CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
+CFLAGS  := -O2 -MMD -Wall -Werror -Wno-unused-result $(INCLUDES) $(CFLAGS)
 LDFLAGS := -O2 $(LDFLAGS)
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
