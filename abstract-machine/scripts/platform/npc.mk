f0c011aed3687e552b41b6161ff87dec6cf065c2
myyerrol <myyerrol@126.com>
2023-04-14 16:34:39 +0800

    feat: add npc config

---
+++ abstract-machine/scripts/platform/npc.mk
@@ -0,0 +1,24 @@
+AM_SRCS := platform/dummy/vme.c \
+           platform/dummy/mpe.c
+
+CFLAGS    += -fdata-sections -ffunction-sections
+LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
+             --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+LDFLAGS   += --gc-sections -e _start
+NPCFLAGS += -l $(shell dirname $(IMAGE).elf)/npc-log.txt
+NPCFLAGS += -e $(IMAGE).elf
+NPCFLAGS += -b
+
+CFLAGS += -DMAINARGS=\"$(mainargs)\"
+.PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
+
+image: $(IMAGE).elf
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+run: image
+	$(MAKE) -C $(NPC_HOME) run PROJECT=cpu_single_cycle ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
+
+gdb: image
+	$(MAKE) -C $(NPC_HOME) gdb PROJECT=cpu_single_cycle ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
