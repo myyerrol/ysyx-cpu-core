1bad4df8a29a62e4b022c5c4ff58338c0e32763f
myyerrol <myyerrol@126.com>
2023-04-15 18:10:01 +0800

    feat: add nemu src func support

---
+++ npc/cpu_single_cycle/srcc/cpu-exec.c
@@ -0,0 +1,8 @@
+#include <common.h>
+#include <isa.h>
+
+CPU_state cpu = {};
+uint64_t g_nr_guest_inst = 0;
+
+void assert_fail_msg() {
+}
--- npc/cpu_single_cycle/srcc/top.c
+++ npc/cpu_single_cycle/srcc/top.c
@@ -11,34 +11,44 @@
 #include "VTop__Dpi.h"
 
 // 存储器函数
-static uint8_t host_mem[CONFIG_MSIZE] PG_ALIGN = {};
+// static uint8_t host_mem[CONFIG_MSIZE] PG_ALIGN = {};
 
-static uint8_t *convertGuestToHost(paddr_t paddr) {
-    return host_mem + paddr - CONFIG_MBASE;
-}
+// static uint8_t *convertGuestToHost(paddr_t paddr) {
+//     return host_mem + paddr - CONFIG_MBASE;
+// }
 
-static void initGuestMemory() {
+// static void initGuestMemory() {
+//     static const uint32_t inst[] = {
+//         0x00100093,
+//         0x00A00193,
+//         0x00100073
+//     };
+//     memcpy(convertGuestToHost(RESET_VECTOR), inst, sizeof(inst));
+// }
+
+// static word_t readHost(void *addr, int len) {
+//     switch (len) {
+//         case 1: return *(uint8_t  *)addr;
+//         case 2: return *(uint16_t *)addr;
+//         case 4: return *(uint32_t *)addr;
+//         IFDEF(CONFIG_ISA64, case 8: return *(uint64_t *)addr);
+//         default: MUXDEF(CONFIG_RT_CHECK, assert(0), return 0);
+//     }
+// }
+
+// static word_t readHostMemory(paddr_t addr, int len) {
+//     word_t ret = readHost(convertGuestToHost(addr), len);
+//     return ret;
+// }
+
+
+void initMem() {
     static const uint32_t inst[] = {
         0x00100093,
         0x00A00193,
         0x00100073
     };
-    memcpy(convertGuestToHost(RESET_VECTOR), inst, sizeof(inst));
-}
-
-static word_t readHost(void *addr, int len) {
-    switch (len) {
-        case 1: return *(uint8_t  *)addr;
-        case 2: return *(uint16_t *)addr;
-        case 4: return *(uint32_t *)addr;
-        IFDEF(CONFIG_ISA64, case 8: return *(uint64_t *)addr);
-        default: MUXDEF(CONFIG_RT_CHECK, assert(0), return 0);
-    }
-}
-
-static word_t readHostMemory(paddr_t addr, int len) {
-    word_t ret = readHost(convertGuestToHost(addr), len);
-    return ret;
+    memcpy(guest_to_host(RESET_VECTOR), inst, sizeof(inst));
 }
 
 // 仿真测试函数
@@ -93,13 +103,15 @@ int main(int argc, char **argv, char **env) {
     if (false && argc && argv && env) {
     }
 
-    initGuestMemory();
+    // initGuestMemory();
+    initMem();
     initSim();
 
     resetSimModule(top, 1);
 
     while (!ebreak_flag) {
-        top->io_iInst = readHostMemory(top->io_oPC, 8);
+        // top->io_iInst = readHostMemory(top->io_oPC, 8);
+        top->io_iInst = paddr_read(top->io_oPC, 8);
         runSimModuleCycle(top);
     }
 
