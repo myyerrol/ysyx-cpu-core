24192ff6e2c40e37c2832675995245bcbc6ee6af
myyerrol <myyerrol@126.com>
2023-04-23 22:08:11 +0800

    feat: add mem byte signal

--- npc/cpu_single_cycle/src/util/Base.scala
+++ npc/cpu_single_cycle/src/util/Base.scala
@@ -13,13 +13,56 @@ object Base {
     val SIGNAL_WIDTH = 10
 
     val INST_NAME_X      = 0.U
-    val INST_NAME_LUI    = 1.U
-    val INST_NAME_AUIPC  = 2.U
-    val INST_NAME_JAL    = 3.U
-    val INST_NAME_JALR   = 4.U
-    val INST_NAME_SD     = 5.U
-    val INST_NAME_ADDI   = 6.U
-    val INST_NAME_EBREAK = 100.U
+    val INST_NAME_SLLI   = 1.U
+    val INST_NAME_SRLI   = 2.U
+    val INST_NAME_SRA    = 3.U
+    val INST_NAME_SRAI   = 4.U
+    val INST_NAME_SLLW   = 5.U
+    val INST_NAME_SLLIW  = 6.U
+    val INST_NAME_SRLW   = 7.U
+    val INST_NAME_SRLIW  = 8.U
+    val INST_NAME_SRAW   = 9.U
+    val INST_NAME_SRAIW  = 10.U
+    val INST_NAME_ADD    = 11.U
+    val INST_NAME_ADDI   = 12.U
+    val INST_NAME_SUB    = 13.U
+    val INST_NAME_LUI    = 14.U
+    val INST_NAME_AUIPC  = 15.U
+    val INST_NAME_ADDW   = 16.U
+    val INST_NAME_ADDIW  = 17.U
+    val INST_NAME_SUBW   = 18.U
+    val INST_NAME_XORI   = 19.U
+    val INST_NAME_OR     = 20.U
+    val INST_NAME_AND    = 21.U
+    val INST_NAME_ANDI   = 22.U
+    val INST_NAME_SLT    = 23.U
+    val INST_NAME_SLTU   = 24.U
+    val INST_NAME_SLTIU  = 25.U
+    val INST_NAME_BEQ    = 26.U
+    val INST_NAME_BNE    = 27.U
+    val INST_NAME_BLT    = 28.U
+    val INST_NAME_BGE    = 29.U
+    val INST_NAME_BLTU   = 30.U
+    val INST_NAME_BGEU   = 31.U
+    val INST_NAME_JAL    = 32.U
+    val INST_NAME_JALR   = 33.U
+    val INST_NAME_LB     = 34.U
+    val INST_NAME_LH     = 35.U
+    val INST_NAME_LBU    = 36.U
+    val INST_NAME_LHU    = 37.U
+    val INST_NAME_LW     = 38.U
+    val INST_NAME_LD     = 39.U
+    val INST_NAME_SB     = 40.U
+    val INST_NAME_SH     = 41.U
+    val INST_NAME_SW     = 42.U
+    val INST_NAME_SD     = 43.U
+    val INST_NAME_EBREAK = 44.U
+    val INST_NAME_MUL    = 45.U
+    val INST_NAME_MULW   = 46.U
+    val INST_NAME_DIVU   = 47.U
+    val INST_NAME_DIVW   = 48.U
+    val INST_NAME_REMU   = 49.U
+    val INST_NAME_REMW   = 50.U
 
     val ALU_TYPE_X      =  0.U(SIGNAL_WIDTH.W)
     val ALU_TYPE_ADD    =  1.U(SIGNAL_WIDTH.W)
@@ -43,6 +86,12 @@ object Base {
 
     val MEM_WR_T  = 1.U(SIGNAL_WIDTH.W)
     val MEM_WR_F  = 0.U(SIGNAL_WIDTH.W)
+
+    val MEM_BYT_X = 0.U(SIGNAL_WIDTH.W)
+    val MEM_BYT_1 = 1.U(SIGNAL_WIDTH.W)
+    val MEM_BYT_2 = 2.U(SIGNAL_WIDTH.W)
+    val MEM_BYT_4 = 3.U(SIGNAL_WIDTH.W)
+    val MEM_BYT_8 = 4.U(SIGNAL_WIDTH.W)
 
     val REG_WR_T  = 1.U(SIGNAL_WIDTH.W)
     val REG_WR_F  = 0.U(SIGNAL_WIDTH.W)
