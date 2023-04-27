13cf95ac6b2461f0e43a230b902850da872187bf
myyerrol <myyerrol@126.com>
2023-04-27 11:27:03 +0800

    fix: fix mem read error

--- npc/cpu_single_cycle/srcv/DPI.v
+++ npc/cpu_single_cycle/srcv/DPI.v
@@ -1,17 +1,19 @@
 module DPI(
     input  wire[31:0] iEbreakFlag,
-    input  wire[63:0] iMemRdAddr,
+    input  wire[63:0] iMemRdAddrInst,
+    input  wire[63:0] iMemRdAddrLoad,
     input  wire[0: 0] iMemWrEn,
     input  wire[63:0] iMemWrAddr,
     input  wire[63:0] iMemWrData,
-    output  reg[63:0] oMemRdData
+    output  reg[63:0] oMemRdDataInst,
+    output  reg[63:0] oMemRdDataLoad
     // input  wire[63:0] iRegVal,
     // output wire[63:0] oRegAddr
 );
 
 import "DPI-C" function void judgeIsEbreak(input int flag);
-import "DPI-C" function longint readMemData(input longint addr);
-import "DPI-C" function void writeMemData(input longint addr, input longint data, input byte len);
+import "DPI-C" function longint readMemData(input longint unsigned addr);
+import "DPI-C" function void writeMemData(input longint unsigned addr , input longint unsigned data, input byte len);
 
 // export "DPI-C" function setRegAddr;
 
@@ -21,14 +23,15 @@ import "DPI-C" function void writeMemData(input longint addr, input longint data
 
 always @(iEbreakFlag) begin
     judgeIsEbreak(iEbreakFlag);
-    // oMemRdData = readMemData(iMemRdAddr);
-    // if (iMemWrEn == 1'b1) begin
-    //     writeMemData(iMemWrAddr, iMemWrData, 8);
-    // end
+    $display("iEbreakFlag: %d\n", iEbreakFlag);
 end
 
-always @(iMemRdAddr) begin
-    oMemRdData = readMemData(iMemRdAddr);
+always @(iMemRdAddrInst) begin
+    oMemRdDataInst = readMemData(iMemRdAddrInst);
+end
+
+always @(iMemRdAddrLoad) begin
+    oMemRdDataLoad = readMemData(iMemRdAddrLoad);
 end
 
 always @(iMemWrEn) begin
