420e0bd39739e3d60357f592a834d6725a0fd876
myyerrol <myyerrol@126.com>
2023-05-03 17:07:38 +0800

    feat: add keyboard ioe

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t key = inl(KBD_ADDR);
+  kbd->keydown = key &  KEYDOWN_MASK;
+  kbd->keycode = key & ~KEYDOWN_MASK;
 }
