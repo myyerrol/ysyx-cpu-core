f8402da58409ba38ae26beb254be1ce414b80117
myyerrol <myyerrol@126.com>
2023-05-03 15:54:35 +0800

    style: add the enter

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -127,6 +127,7 @@ void cpu_exec(uint64_t n) {
 
     case NEMU_END: case NEMU_ABORT:
 #ifdef CONFIG_ITRACE_COND
+      printf("\n");
       itrace_display();
 #endif
 #ifdef CONFIG_MTRACE_COND_RESULT
