5d35e936e1ec72ce08862b182e856921030dc6cf
myyerrol <myyerrol@126.com>
2023-05-04 19:36:49 +0800

    feat: add malloc func

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,12 +29,28 @@ int atoi(const char* nptr) {
   return x;
 }
 
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+static char *addr_curr_p = NULL;
+#endif
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  if (addr_curr_p == NULL) {
+    addr_curr_p = (void *)ROUNDUP(heap.start, 8);
+  }
+
+  size = (size_t)ROUNDUP(size, 8);
+  char *addr_prev_p = addr_curr_p;
+  addr_curr_p += size;
+  assert((uintptr_t)heap.start <= (uintptr_t)addr_curr_p &&
+         (uintptr_t)addr_curr_p < (uintptr_t)heap.end);
+  for (uint64_t *p = (uint64_t *)addr_prev_p; p != (uint64_t *)addr_curr_p; p++) {
+    *p = 0;
+  }
+  return addr_prev_p;
 #endif
   return NULL;
 }
