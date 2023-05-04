84843eae48be97c880946ed570b81d78fcdb1afc
myyerrol <myyerrol@126.com>
2023-05-04 10:44:31 +0800

    feat: add condition build

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,8 +1,8 @@
 #include <am.h>
 #include <nemu.h>
-#include <klib.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define CONFIG_GPU_TEST 0
 
 static uint32_t width  = 0;
 static uint32_t height = 0;
@@ -11,18 +11,17 @@ void __am_gpu_init() {
   uint32_t gpu_cfg = inl(VGACTL_ADDR);
   width  = (gpu_cfg & 0xffff0000) >> 16;
   height = (gpu_cfg & 0x0000ffff);
-  // int i;
-  // int w = width;
-  // int h = height;
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i++) fb[i] = i;
-  // outl(SYNC_ADDR, 1);
+#if CONFIG_GPU_TEST
+  int i;
+  int w = width;
+  int h = height;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
+#endif
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t gpu_cfg = inl(VGACTL_ADDR);
-  uint32_t width  = (gpu_cfg & 0xffff0000) >> 16;
-  uint32_t height = (gpu_cfg & 0x0000ffff);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = width, .height = height,
@@ -59,9 +58,11 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     fb[index] = pixels[i];
   }
 
+#if !CONFIG_GPU_TEST
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
+#endif
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
