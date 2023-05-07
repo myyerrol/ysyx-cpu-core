32265938e6d7cd4b62bbb2bbd91bb4069457f9c2
myyerrol <myyerrol@126.com>
2023-05-07 20:00:22 +0800

    feat: add reg print func

--- npc/cpu_single_cycle/srcc/reg.h
+++ npc/cpu_single_cycle/srcc/reg.h
@@ -1,6 +1,8 @@
 #ifndef __REG_H__
 #define __REG_H__
 
+#include "common.h"
+
 void isa_reg_display();
 
 #endif
--- npc/cpu_single_cycle/srcc/sim.h
+++ npc/cpu_single_cycle/srcc/sim.h
@@ -3,8 +3,12 @@
 
 #include <verilated.h>
 #include <verilated_vcd_c.h>
-#include "VTop.h"
-#include "VTop__Dpi.h"
+#include <VTop.h>
+#include <VTop__Dpi.h>
+
+extern VerilatedContext *contextp;
+extern VerilatedVcdC    *tfp;
+extern VTop             *top;
 
 void runSimStep();
 void exitSim();
