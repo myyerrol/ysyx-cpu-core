61c076f32f33566a76d7fffca138ff58745a2230
myyerrol <myyerrol@126.com>
2023-06-01 16:12:36 +0800

    style: modify irq text

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: {
-      printf("[irq] event: yeild\n"); break;
+      printf("[irq] event hit: yeild\n"); break;
     }
     default: panic("Unhandled event ID = %d", e.event);
   }
