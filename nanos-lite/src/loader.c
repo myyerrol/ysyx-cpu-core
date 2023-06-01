42751d0dceb727e61a2948592e606e74f139cf8b
myyerrol <myyerrol@126.com>
2023-06-01 16:12:47 +0800

    feat: add load func

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,26 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr elf_header;
+  ramdisk_read(&elf_header, 0, sizeof(elf_header));
+  Elf_Phdr elf_segment_arr[elf_header.e_phnum];
+  ramdisk_read(elf_segment_arr,
+               elf_header.e_phoff,
+               sizeof(Elf_Phdr) * elf_header.e_phnum);
+  for (int i = 0; i < elf_header.e_phnum; i++) {
+    if (elf_segment_arr[i].p_type == PT_LOAD) {
+      ramdisk_read((void *)elf_segment_arr[i].p_vaddr,
+                           elf_segment_arr[i].p_offset,
+                           elf_segment_arr[i].p_memsz);
+      memset((void *)(elf_segment_arr[i].p_vaddr + elf_segment_arr[i].p_filesz),
+             0,
+             elf_segment_arr[i].p_memsz - elf_segment_arr[i].p_filesz);
+    }
+  }
+  return elf_header.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -19,13 +19,15 @@ void hello_fun(void *arg) {
   }
 }
 
+extern void naive_uload(PCB *pcb, const char *filename);
+
 void init_proc() {
   switch_boot_pcb();
 
   Log("Initializing processes...");
 
   // load program here
-
+  naive_uload(NULL, NULL);
 }
 
 Context* schedule(Context *prev) {
