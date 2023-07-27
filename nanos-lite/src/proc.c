7cedc75be3447d615a044a704b5c58b4d33a4f40
myyerrol <myyerrol@126.com>
2023-07-27 16:27:04 +0800

    feat: update the program list

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,18 +27,18 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  // naive_uload(NULL,"/bin/dummy");
-  // naive_uload(NULL,"/bin/hello");
-  // naive_uload(NULL,"/bin/file-test");
-  // naive_uload(NULL,"/bin/timer-test");
-  // naive_uload(NULL,"/bin/event-test");
-  // naive_uload(NULL,"/bin/bmp-test");
-  // naive_uload(NULL,"/bin/fixedptc-test");
-  // naive_uload(NULL,"/bin/nslider");
-  // naive_uload(NULL,"/bin/menu");
-  // naive_uload(NULL,"/bin/nterm");
-  naive_uload(NULL, "/bin/bird");
-  // naive_uload(NULL, "/bin/pal");
+  // naive_uload(NULL, "/bin/dummy");
+  // naive_uload(NULL, "/bin/hello");
+  // naive_uload(NULL, "/bin/file-test");
+  // naive_uload(NULL, "/bin/timer-test");
+  // naive_uload(NULL, "/bin/event-test");
+  // naive_uload(NULL, "/bin/bmp-test");
+  // naive_uload(NULL, "/bin/fixedptc-test");
+  // naive_uload(NULL, "/bin/nslider");
+  // naive_uload(NULL, "/bin/menu");
+  // naive_uload(NULL, "/bin/nterm");
+  // naive_uload(NULL, "/bin/bird");
+  naive_uload(NULL, "/bin/pal");
 }
 
 Context* schedule(Context *prev) {
