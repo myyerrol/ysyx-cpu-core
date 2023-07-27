6ae1747a65b7fb6a83eaf7718e19a0db199178be
myyerrol <myyerrol@126.com>
2023-07-27 16:26:38 +0800

    feat: update the program

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -17,7 +17,8 @@ void __am_gpu_init() {
   int h = height;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   // for (i = 0; i < w * h; i++) fb[i] = i;
-  for (i = 0; i < w * h; i++) fb[i] = 0x00ffffff;
+  // for (i = 0; i < w * h; i++) fb[i] = 0x00ffffff;
+    for (i = 0; i < w * h; i++) fb[i] = 0x00000000;
   outl(SYNC_ADDR, 1);
 }
 
