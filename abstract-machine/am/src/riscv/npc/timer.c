933fd6fe2bc04eaf20f6d9539e5a1b2ef42cfdce
myyerrol <myyerrol@126.com>
2023-11-03 19:46:44 +0800

    fix: fix timer bug

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -7,8 +7,8 @@
 static uint64_t boot_time = 0;
 
 static uint64_t read_time() {
-  uint32_t lo = inl(RTC_ADDR + 0);
   uint32_t hi = inl(RTC_ADDR + 4);
+  uint32_t lo = inl(RTC_ADDR + 0);
   uint64_t time = ((uint64_t)hi << 32) | lo;
   return time;
 }
