411ae79fa2f90dee337a3606303627c051a025a9
myyerrol <myyerrol@126.com>
2023-11-21 09:36:55 +0800

    chore: delete unused codes

--- npc/cpu/multip/srcv/DPI.v
+++ npc/cpu/multip/srcv/DPI.v
@@ -1,16 +1,14 @@
 module DPI(
-    input wire[07:0] iEbreakFlag,
-    input wire[63:0] iMemRdAddrInst,
-    input wire[63:0] iMemRdAddrLoad,
-    input wire[00:0] iMemWrEn,
-    input wire[63:0] iMemWrAddr,
-    input wire[63:0] iMemWrData,
-    input wire[07:0] iMemWrLen,
-    // input wire[63:0] iGPRData,
+    input  wire[07:0] iEbreakFlag,
+    input  wire[63:0] iMemRdAddrInst,
+    input  wire[63:0] iMemRdAddrLoad,
+    input  wire[00:0] iMemWrEn,
+    input  wire[63:0] iMemWrAddr,
+    input  wire[63:0] iMemWrData,
+    input  wire[07:0] iMemWrLen,
 
-    output reg[63:0] oMemRdDataInst,
-    output reg[63:0] oMemRdDataLoad
-    // output reg[63:0] oGPRAddr
+    output  reg[63:0] oMemRdDataInst,
+    output  reg[63:0] oMemRdDataLoad
 );
 
 import "DPI-C" context function void judgeIsEbreak(input byte unsigned flag);
@@ -23,13 +21,6 @@ import "DPI-C" context function longint unsigned readMemData(
 import "DPI-C" context function void writeMemData(input longint unsigned addr,
                                                   input longint unsigned data,
                                                   input byte unsigned len);
-
-// export "DPI-C" function getISAGPRData;
-
-// function longint unsigned getISAGPRData(longint unsigned addr);
-//     oGPRAddr = addr;
-//     return iGPRData;
-// endfunction
 
 always @(iEbreakFlag) begin
     judgeIsEbreak(iEbreakFlag);
