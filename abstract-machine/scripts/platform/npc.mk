48c52d1790f714ba09b9a614ae67b8454bf133df
myyerrol <myyerrol@126.com>
2023-12-27 11:13:39 +0800

    fix: fix the bug of npc input parameters being replaced

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -1,12 +1,12 @@
 AM_SRCS := platform/dummy/vme.c \
            platform/dummy/mpe.c
 
-CFLAGS   += -fdata-sections -ffunction-sections
-LDFLAGS  += -T $(AM_HOME)/scripts/linker.ld \
+CFLAGS     += -fdata-sections -ffunction-sections
+LDFLAGS    += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
-LDFLAGS  += --gc-sections -e _start
-NPCFLAGS += -l $(shell dirname $(IMAGE).elf)/npc-log.txt
-NPCFLAGS += -e $(IMAGE).elf
+LDFLAGS    += --gc-sections -e _start
+ARGS_EMBED += -l $(shell dirname $(IMAGE).elf)/npc-log.txt
+ARGS_EMBED += -e $(IMAGE).elf
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
@@ -17,7 +17,7 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	$(MAKE) -C $(NPC_HOME) run ARGS="$(NPCFLAGS) $(ARGS)" IMG=$(IMAGE).bin PROJECT=$(PROJECT)
+	$(MAKE) -C $(NPC_HOME) run ARGS="$(ARGS_EMBED) $(ARGS)" IMG=$(IMAGE).bin PROJECT=$(PROJECT)
 
 gdb: image
-	$(MAKE) -C $(NPC_HOME) gdb ARGS="$(NPCFLAGS) $(ARGS)" IMG=$(IMAGE).bin PROJECT=$(PROJECT)
+	$(MAKE) -C $(NPC_HOME) gdb ARGS="$(ARGS_EMBED) $(ARGS)" IMG=$(IMAGE).bin PROJECT=$(PROJECT)
